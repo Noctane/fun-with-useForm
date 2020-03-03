cdd1a3c6311def3300c72bef3aa317246fb1bf1b
Arnaud Prat <arnaud.prat@gmail.com>
2020-03-03 20:55:56 +0100

    add minor style changes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Sidebar from '../Sidebar';
-
 const Grid = styled.div`
   height: 100vh;
   padding: 0;
   margin: 0;
-  display: grid;
-  grid-template-columns: 1fr 4fr;
 `;
 
 function Layout({children}) {
   return(
     <Grid>
-      <Sidebar>hello</Sidebar>
-      <div>{children}</div>
+      {children}
     </Grid>
   )
 }
--- src/containers/LoginForm/index.js
+++ src/containers/LoginForm/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import styled from 'styled-components';
+import colors from '../../assets/colors';
 // Hooks
 import useForm from './hooks/useForm';
 // Validation rules
@@ -10,6 +11,7 @@ import Form from './components/Form';
 import Field from './components/Field';
 import Button from './components/Button';
 import SecondaryButton from './components/SecondaryButton';
+import Title from '../../components/Sidebar/Title';
 
 const FlexBlock = styled.div`
   display: flex;
@@ -17,22 +19,36 @@ const FlexBlock = styled.div`
   margin-top: 1.5em;
 `;
 
+const Span = styled.span`
+  font-size: 1rem;
+  display: block;
+  color: ${colors.greyLite};
+`;
+
 function LoginForm() {
   const { values, handlers, errors } = useForm(login, validate);
 
   const [busy, setBusy] = useState(false);
+  const [credentials, setCredentials] = useState({});
 
   function login() {
     setBusy(true);
     setTimeout(() =>{
       console.log('Credentials sent ! yay !', values);
       setBusy(false);
+      setCredentials(values);
       handlers.reset();
     }, 3000);
   }
 
+  const reset = () => {
+    setCredentials({});
+    handlers.reset();
+  }
+
   return (
     <Fragment>
+      <div><Title><Span>Fun with</Span> Forms</Title></div>
       <H2>Login Form</H2>
       <Form onSubmit={handlers.handleSubmit}>
         <Field
@@ -60,10 +76,11 @@ function LoginForm() {
           isRequired
         />
         <FlexBlock>
-          <SecondaryButton onClick={handlers.reset}>Reset</SecondaryButton>
+          <SecondaryButton onClick={reset}>Reset</SecondaryButton>
           <Button type="submit" busy={busy}>Login</Button>
         </FlexBlock>
       </Form>
+      <pre>{JSON.stringify(credentials)}</pre>
     </Fragment>
   );
 }
